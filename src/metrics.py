42d38cc770dc6e48c0061ba9e0f36e90e8d4d44c
franyober <franyober123@hotmail.com>
2024-08-29 01:24:15 -0500

    asignando una sola funcion para estraer las metricas

--- src/metrics.py
+++ src/metrics.py
@@ -2,13 +2,17 @@ import requests
 from datetime import datetime
 
 def get_metrics(*args):
-    status_codes = []
-    response_times = []
+    status_codes= []
+    response_times= []
+    sizes= []
+    types = []
 
     for i in args:
         r= requests.get(i)
         status_codes.append(r.status_code)
         response_times.append(r.elapsed.total_seconds()*1000) # tiempo de respuesta en milisegundos
+        sizes.append(len(r.content))
+        types.append(r.headers['content-type'].split(";")[0]) # tipo de contenido
 
         # modificando y usando el formato de la fecha
         date= r.headers['date'] # resultado en string
@@ -18,8 +22,13 @@ def get_metrics(*args):
         minute= newDate.minute
         hour= newDate.hour
 
-    return status_codes, response_times, onlydate, f"{hour}:{minute}"
+    return status_codes, response_times, str(onlydate), f"{hour}:{minute}", sizes, types
+
+
 
 
 if __name__ == "__main__":
-    print("jello")
+
+    url= "https://calculos-energeticos.netlify.app/_astro/hoisted.D_3o3WCS.js"
+    metrics = get_metrics(url)
+    print(metrics)
